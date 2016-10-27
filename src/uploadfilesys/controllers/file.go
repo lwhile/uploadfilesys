e99e7463878b77bdea50f4211d9603c9a2d4881f
lwh <lwhile@outlook.com>
2016-10-27 23:28:02 +0800

    add some static file

---
+++ src/uploadfilesys/controllers/file.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "github.com/astaxie/beego"
+
+type FileController struct {
+    beego.Controller
+}
+
+func (this *FileController) PostFile() {
+
+}
--- src/uploadfilesys/main.go
+++ src/uploadfilesys/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+    beego.Router("/file")
 	beego.Run()
 }
 
